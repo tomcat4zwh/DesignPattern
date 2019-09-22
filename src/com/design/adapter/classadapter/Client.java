4cde9999fc7c980ade6c89538da6171aa1dc976a
zwh <zwh@163.com>
2019-09-22 16:23:25 +0800

    test for hot fix

--- src/com/design/adapter/classadapter/Client.java
+++ src/com/design/adapter/classadapter/Client.java
@@ -5,6 +5,7 @@ public class Client {
 	public static void main(String[] args) {
 		Phone phone = new Phone();
 		phone.charging(new VoltageAdapter());
+		System.out.println("hot fix..............");
 	}
 
 }
